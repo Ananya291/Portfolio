5370aaeff639871e5281119f1abb95920532e45e
Ananya291 <ananya27042000@gmail.com>
2021-07-19 23:56:41 +0530

    navbar files

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./LandingPage.css"
+
+function LandingPage() {
+    return (
+        <div className="landing_page">
+            heyyy jdasgWHFCKHFHEklbcuiAG8IO  no nahi aaya
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./NavBar.css"
+
+function NavBar() {
+    var name = "<AT />"
+    return (
+        // <div className="container-fluid">
+        <nav class="navbar navbar-expand-lg fixed-top navbar-light">
+            <a className="navbar-brand" href="#">{name}</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+
+                <ul class="navbar-nav navi">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About Me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Experience</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        // </div>
+    )
+}
+
+export default NavBar
